1cad2a2551e080a16f1a17e8052eebce56f8eabd
finbarr12 <finarrogbonna@gmail.com>
2023-08-01 00:58:42 +0100

    addedmailer

--- env/EnvironmentVariables.ts
+++ env/EnvironmentVariables.ts
@@ -4,4 +4,8 @@ dotenv.config();
 
 export const EnvironmentVariables = {
   Port: process.env.Port,
+  GOOGLE_ID: process.env.GOOGLE_ID,
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+  GOOGLE_REFRESHTOKEN: process.env.GOOGLE_REFRESHTOKEN,
+  GOOGLE_REDIRECT: process.env.GOOGLE_REDIRECT,
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import cors from "cors";
 import express, { Application } from "express";
 import { EnvironmentVariables } from "./env/EnvironmentVariables";
+import { google } from "googleapis";
+import * as nodemailer from "nodemailer";
 
 const app: Application = express();
 
 const Port = EnvironmentVariables.Port;
 
 app.use(express.json()).use(cors());
+
+const GOOGLE_ID = EnvironmentVariables.GOOGLE_ID;
+const GOOGLE_SECRET = EnvironmentVariables.GOOGLE_SECRET;
+const GOOGLE_REFRESHTOKEN = EnvironmentVariables.GOOGLE_REFRESHTOKEN;
+const GOOGLE_REDIRECT = EnvironmentVariables.GOOGLE_REDIRECT;
+
+const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT);
+oAuth.setCredentials({ access_token: GOOGLE_REFRESHTOKEN });
 
 app.listen(Port, () => {
   console.log(`Server is running on port ${Port}`);
