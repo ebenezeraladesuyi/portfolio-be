b4ed21e9cb4498c21e65015e08d3f8831eef2dc7
finbarr12 <finarrogbonna@gmail.com>
2023-08-01 02:18:29 +0100

    added

--- env/EnvironmentVariables.ts
+++ env/EnvironmentVariables.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const EnvironmentVariables = {
-  Port: process.env.Port,
   GOOGLE_ID: process.env.GOOGLE_ID,
   GOOGLE_SECRET: process.env.GOOGLE_SECRET,
   GOOGLE_REFRESHTOKEN: process.env.GOOGLE_REFRESHTOKEN,
   GOOGLE_REDIRECT: process.env.GOOGLE_REDIRECT,
+  accessTOKEN: process.env.accessTOKEN,
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { EnvironmentVariables } from "./env/EnvironmentVariables";
 import { google } from "googleapis";
 import * as nodemailer from "nodemailer";
 
 const app: Application = express();
-
-const Port = EnvironmentVariables.Port;
 
 app.use(express.json()).use(cors());
 
@@ -18,6 +16,43 @@ const GOOGLE_REDIRECT = EnvironmentVariables.GOOGLE_REDIRECT;
 const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT);
 oAuth.setCredentials({ access_token: GOOGLE_REFRESHTOKEN });
 
-app.listen(Port, () => {
-  console.log(`Server is running on port ${Port}`);
+app.post("/mailer", async (req: Request, res: Response) => {
+  const { email, messages, subject } = req.body;
+
+  try {
+    const getAccessToken: any = (await oAuth.getAccessToken()).token;
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "ogbonnafinbarr@gmail.com",
+        clientId: GOOGLE_ID,
+        clientSecret: GOOGLE_SECRET,
+        refreshToken: GOOGLE_REFRESHTOKEN,
+        accessToken: EnvironmentVariables.accessTOKEN,
+      },
+    });
+
+    const message = {
+      from: email,
+      to: "ogbonnafinbarr@gmail.com",
+      subject: subject,
+      text: messages,
+    };
+
+    transport.sendMail(message);
+    res.status(400).json({
+      message: "Mail sent",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error occured",
+      data: error,
+    });
+  }
+});
+
+app.listen(2004, () => {
+  console.log(`Server is running on port 2004`);
 });
